ee2ae10aa74b3d4cd3b30426b8c30758c20d9206
Sebastian <sebasgrammar@hotmail.com>
2020-02-03 23:25:45 -0500

    added function to create rows

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(".input")
 const submit = document.querySelector(".submit")
+const results = document.querySelector(".results")
 
 function findWords(text) {
     return text.value.split(/[\W]/).filter(word => word)
@@ -8,13 +9,47 @@ function findWords(text) {
 function countWords(array) {
     let words = {}
     for (let word of array) {
-        words[word] ? words[word] ++ : words[word] = 1
+        words[word] ? words[word]++ : words[word] = 1
     }
     return words
 }
 
-submit.addEventListener("click", function() {
-    console.log(countWords(findWords(input)))
+/* CONVOLUTED ON PURPOSE - I'M GOING TO REFACTOR LATER ON. FOR NOW, I'LL JUST TEST A FEW THINGS */
+
+let elements = {
+    p: 4
+}
+
+function test(thing) {
+    let row = document.createElement("div")
+    for (let element in thing) {
+        let counter = 0;
+        while (counter < thing[element]) {
+            let object = document.createElement(element)
+            object.textContent = counter
+            row.appendChild(object)
+            counter ++
+        }
+    }
+    return row
+}
+
+function createRow(container, child) {
+    container.appendChild(child)
+}
+
+function createTable(object, container) {
+    for (let word in object) {
+        createRow(container, test(elements))
+    }
+}
+
+/************************************/
+
+submit.addEventListener("click", function () {
+    //console.log(countWords(findWords(input)))
+    console.log(test(countWords(findWords(input))))
+    //createTable(countWords(findWords(input)), results)
 })
 
 //console.log(input.split(" "))
