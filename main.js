2a9c146f0a2de1fb5437eb820b2fe5e6f6e603ba
Sebastian <sebasgrammar@hotmail.com>
2020-02-10 22:24:45 -0500

    added rank of most common words

--- main.js
+++ main.js
@@ -17,35 +17,18 @@ function countWords(array) {
     for (let word of array) {
         words[word] ? words[word] ++ : words[word] = 1
     }
-    console.log(words) //.sort((x, y) => {return x - y}))
-    return words //.sort((x, y) => {return x - y})
+    return words
 }
 
 function sortObject(object) {
-    console.log(Object.keys(object).sort((x, y) => {return object[y] - object[x]}))
+    return Object.keys(object).sort((x, y) => {return object[y] - object[x]})
 }
-
-/* CONVOLUTED ON PURPOSE - I'M GOING TO REFACTOR LATER ON. FOR NOW, I'LL JUST createElement A FEW THINGS */
 
 let elements = {
     p: 3
 }
 
-// function createElement({rank, count, word}) {
-//     let oe = document.createElement("div")
-//     oe.classList.add("oe")
-//     rank.appendChild(oe)
-    
-// }
-
-// function createElement(object) {
-//     for (let element in object) {
-//         let property = document.createElement("p")
-//         property.textContent = element
-//         object[element].appendChild(property)
-//     }
-// }
-
+/*
 function createElement(object, {count, word}) {
 
     for (let property in object) {
@@ -63,15 +46,39 @@ function createElement(object, {count, word}) {
     }
     
 }
+*/
 
+function createElement(array, {count, word, rank}, object) {
+    console.log(object)
+    let index = 1
 
+    for (let element of array) {
+
+        let key = document.createElement("p")
+        key.textContent = element
+        word.appendChild(key)
+
+        let value = document.createElement("p")
+        value.textContent = object[element]
+        count.appendChild(value)
+
+        let position = document.createElement("p")
+        position.textContent = index
+        rank.appendChild(position)
+
+        index ++
+    }
+    
+}
 
 /************************************/
 
 submit.addEventListener("click", function () {
     //console.log(countWords(findWords(input)))
-    sortObject(countWords(findWords(input)))
-    createElement(countWords(findWords(input)), row)
+    //console.log(sortObject(countWords(findWords(input))))
+    let sortedObject = sortObject(countWords(findWords(input)))
+
+    createElement(sortedObject, row, countWords(findWords(input)))
     
     //createElement(row)
 })
